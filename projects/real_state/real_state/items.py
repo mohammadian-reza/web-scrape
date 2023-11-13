b247cd90fb4d066f9316aa0e98c12685bd8daa27
mohammadian-reza <mohammadian.tbz007@gmail.com>
2023-11-13 15:59:39 +0330

    WebScrape project: Extracting data

---
+++ projects/real_state/real_state/items.py
@@ -0,0 +1,26 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, Join 
+
+def description_in(d):
+    return d.strip()
+    
+def description_out(d):
+    labels = d[0:3]
+    values = d[3:]
+    output = {
+        labels[0] : "".join(values[0]),
+        labels[1] : " ".join(values[1:-1]),
+        labels[2] : "".join(values[-1])
+    }
+    return output
+
+class RealEstateItem(scrapy.Item):
+    name = scrapy.Field(output_processor=Join())
+    description = scrapy.Field(input_processor= MapCompose(description_in), output_processor = description_out)
+    price = scrapy.Field(output_processor=Join())
+    agency = scrapy.Field(output_processor=Join())
---
+++ projects/real_state/real_state/spiders/listing.py
@@ -0,0 +1,36 @@
+import scrapy
+from real_state.items import RealEstateItem
+from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor 
+
+class ListingSpider(CrawlSpider):
+    name = "listing"
+    allowed_domains = ["arizonarealestate.com"]
+    start_urls = [
+                  "https://arizonarealestate.com"
+]
+    rules = (
+        Rule(
+            LinkExtractor(restrict_xpaths=(
+                "//section[@class='section-city-list']"
+            )),
+            callback = 'parse',
+            follow = True
+        ),
+    )
+
+    def parse(self, response):
+        gallery = response.xpath('//div[@class="si-listings-column"]')
+        for listing in gallery:
+            item = ItemLoader(item=RealEstateItem(), response=response, selector=listing)
+            item.add_xpath('name', './/div[@class="si-listing__title-main"]/text()')
+            item.add_xpath('name', './/div[@class="si-listing__neighborhood"]/span[@class="si-listing__neighborhood-place"]/text()')
+            item.add_xpath('description', './/div[@class="si-listing__info"]//div[@class="si-listing__info-label"]/text()')
+            item.add_xpath('description', './/div[@class="si-listing__info"]//div[@class="si-listing__info-value"]/descendant::*/text()')
+            item.add_xpath('price', './/div[@class="si-listing__photo-price"]/span/text()')
+            item.add_xpath('agency', './/div[@class="si-listing__footer"]/div/text()')
+            yield item.load_item()
+            next_page = response.xpath('//a[@class="js-page-link"]/@href').get()
+            if next_page:
+                yield response.follow(next_page, callback=self.parse)
